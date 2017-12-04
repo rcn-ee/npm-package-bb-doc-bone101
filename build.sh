54065b5b34ad352b277bb6ab630b48d7e37eca31
Robert Nelson <robertcnelson@gmail.com>
2017-12-04 14:16:21 -0600

    bone101: copy bonesript npm changes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -16,21 +16,30 @@ distro=$(lsb_release -cs)
 
 npm_git_install () {
 	if [ -d /usr/local/lib/node_modules/${npm_project}/ ] ; then
+		echo "Resetting: /usr/local/lib/node_modules/${npm_project}/"
 		rm -rf /usr/local/lib/node_modules/${npm_project}/ || true
 	fi
 
 	if [ -d /tmp/${git_project}/ ] ; then
+		echo "Resetting: /tmp/${git_project}/"
 		rm -rf /tmp/${git_project}/ || true
 	fi
 
 	git clone -b ${git_branch} ${git_user}/${git_project} /tmp/${git_project}
 	if [ -d /tmp/${git_project}/ ] ; then
+		echo "Cloning: ${git_user}/${git_project}"
 		cd /tmp/${git_project}/
 		package_version=$(cat package.json | grep version | awk -F '"' '{print $4}' || true)
 		git_version=$(git rev-parse --short HEAD)
-		TERM=dumb ${node_bin} ${npm_bin} install -g ${npm_options}
-		cd -
-		rm -rf /tmp/${git_project}/
+
+		cd /tmp/
+		echo "TERM=dumb ${node_bin} ${npm_bin} pack ${npm_project}/"
+		tmp_package=$(TERM=dumb ${node_bin} ${npm_bin} pack ${npm_project}/ | tail -1)
+
+		echo "TERM=dumb ${node_bin} ${npm_bin} install -g ${tmp_package} ${npm_options}"
+		TERM=dumb ${node_bin} ${npm_bin} install -g ${tmp_package} ${npm_options}
+
+		cd $DIR/
 	fi
 
 	wfile="${npm_project}-${package_version}-${git_version}-${node_version}"
@@ -38,12 +47,16 @@ npm_git_install () {
 	if [ -f ${wfile}.tar.xz ] ; then
 		rm -rf ${wfile}.tar.xz || true
 	fi
-	tar -cJf ${wfile}.tar.xz ${npm_project}/
+	tar -hcJf ${wfile}.tar.xz ${npm_project}/
 	cd -
 
 	if [ ! -f ./deploy/${distro}/${wfile}.tar.xz ] ; then
 		cp -v /usr/local/lib/node_modules/${wfile}.tar.xz ./deploy/${distro}/
 		echo "New Build: ${wfile}.tar.xz"
+	fi
+
+	if [ -d /tmp/${git_project}/ ] ; then
+		rm -rf /tmp/${git_project}/
 	fi
 }
 
@@ -52,14 +65,21 @@ npm_pkg_install () {
 		rm -rf /usr/local/lib/node_modules/${npm_project}/ || true
 	fi
 
-	TERM=dumb ${node_bin} ${npm_bin} install -g ${npm_options} ${npm_project}@${package_version}
+	cd /tmp/
+	echo "TERM=dumb ${node_bin} ${npm_bin} pack ${npm_project}@${package_version}/"
+	tmp_package=$(TERM=dumb ${node_bin} ${npm_bin} pack ${npm_project}@${package_version}/ | tail -1)
+
+	echo "TERM=dumb ${node_bin} ${npm_bin} install -g ${tmp_package} ${npm_options}"
+	TERM=dumb ${node_bin} ${npm_bin} install -g ${tmp_package} ${npm_options}
+
+	cd $DIR/
 
 	wfile="${npm_project}-${package_version}-${node_version}"
 	cd /usr/local/lib/node_modules/
 	if [ -f ${wfile}.tar.xz ] ; then
 		rm -rf ${wfile}.tar.xz || true
 	fi
-	tar -cJf ${wfile}.tar.xz ${npm_project}/
+	tar -hcJf ${wfile}.tar.xz ${npm_project}/
 	cd -
 
 	if [ ! -f ./deploy/${distro}/${wfile}.tar.xz ] ; then
@@ -73,7 +93,11 @@ npm_install () {
 	npm_bin="/usr/bin/npm"
 
 	unset node_version
-	node_version=$(/usr/bin/nodejs --version || true)
+	node_version=$(${node_bin} --version || true)
+
+	unset npm_version
+	npm_version=$(${node_bin} ${npm_bin} --version || true)
+
 
 	echo "npm: [`${node_bin} ${npm_bin} --version`]"
 	echo "node: [`${node_bin} --version`]"
